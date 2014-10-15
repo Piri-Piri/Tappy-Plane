fe5053703a0826df424588e17b3c9c43b2a4ee50
Piri-Piri <david.pirih@gmx.de>
2014-10-15 22:06:22 +0200

    add puff effect and world gravity and force apply by touch

--- Tappy Plane/GameScene.swift
+++ Tappy Plane/GameScene.swift
@@ -16,23 +16,40 @@ class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
+        // Setup Physics
+        self.physicsWorld.gravity = CGVectorMake(0.0, -5.5)
+        
+        // Setup World
         world = SKNode()
         self.addChild(world)
         
+        // Setup Player
         player = Plane()
         player.position = CGPointMake(view.frame.size.width * 0.5, view.frame.size.height * 0.5)
+        player.physicsBody?.affectedByGravity = false
         world.addChild(player)
+        
+        player.isEngineRunning = true
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-           self.player.setRandomColor()
+            player.physicsBody?.affectedByGravity = true
+           self.player.isAccelerating = true
+        
+        }
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        for touch: AnyObject in touches {
+            self.player.isAccelerating = false
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        self.player.update()
     }
 }
--- Tappy Plane/Plane.swift
+++ Tappy Plane/Plane.swift
@@ -11,11 +11,37 @@ import SpriteKit
 
 class Plane: SKSpriteNode {
     
+    let kPlanAnimationKey: String = "PlaneAnimation"
+    
     var planeAnimations: NSMutableArray!
+    var isEngineRunning: Bool = false {
+        didSet {
+            /* avoid that puff effects is follows the plane (looks not realistic) */
+            self.puffTrailEmitter.targetNode = self.parent
+            if isEngineRunning {
+                self.actionForKey(kPlanAnimationKey)?.speed = 1.0
+                self.puffTrailEmitter.particleBirthRate = puffTrailBirthRate
+            }
+            else {
+                self.actionForKey(kPlanAnimationKey)?.speed = 0.0
+                self.puffTrailEmitter.particleBirthRate = 0.0
+            }
+        }
+    }
+    var isAccelerating: Bool = false
+    
+    var puffTrailEmitter:SKEmitterNode!
+    var puffTrailBirthRate:CGFloat = 0.0
+    
+    
     
     override init() {
         let plane = SKTexture(imageNamed: "planeBlue1@2x")
         super.init(texture: plane, color: UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.0), size: plane.size())
+        
+        // Setup PhysicsBody
+        self.physicsBody = SKPhysicsBody(circleOfRadius: self.size.width * 0.5)
+        self.physicsBody?.mass = 0.08
         
         // Init Array to hold animation actions
         planeAnimations = NSMutableArray()
@@ -28,6 +54,16 @@ class Plane: SKSpriteNode {
             let action = self.animationFromArray(animations.objectForKey(key) as NSArray, withDuration: 0.4)
             planeAnimations.addObject(action)
         }
+        
+        // Setup puff trail particle effect
+        let particleFile = NSBundle.mainBundle().pathForResource("PlanePuffTrail", ofType: "sks")
+        puffTrailEmitter = NSKeyedUnarchiver.unarchiveObjectWithFile(particleFile!) as SKEmitterNode
+        puffTrailEmitter.position = CGPointMake(-self.size.width * 0.5, -5)
+        self.addChild(puffTrailEmitter)
+        self.puffTrailBirthRate = puffTrailEmitter.particleBirthRate
+        // initial value
+        puffTrailEmitter.particleBirthRate = 0
+        
         setRandomColor()
         
     }
@@ -52,7 +88,18 @@ class Plane: SKSpriteNode {
     }
     
     func setRandomColor() {
-        self.runAction(planeAnimations.objectAtIndex(Int(arc4random_uniform(UInt32(planeAnimations.count)))) as SKAction)
+        self.removeActionForKey(kPlanAnimationKey)
+        let animation = planeAnimations.objectAtIndex(Int(arc4random_uniform(UInt32(planeAnimations.count)))) as SKAction
+        self.runAction(animation, withKey: kPlanAnimationKey)
+        if !isEngineRunning {
+            self.actionForKey(kPlanAnimationKey)?.speed = 0.0
+        }
+    }
+    
+    func update() {
+        if isAccelerating {
+            self.physicsBody?.applyForce(CGVectorMake(0.0, 100.0))
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
