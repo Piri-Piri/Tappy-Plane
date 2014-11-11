60581a2bbf084cd360e59a7932ba35a0bbd4b002
Piri-Piri <david.pirih@gmx.de>
2014-11-11 23:34:43 +0100

    class for setting (gamemode and controlls)

---
+++ Tappy Plane/SettingsMenuScene.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsMenuScene.swift
+//  Tappy Plane
+//
+//  Created by David Pirih on 11.11.14.
+//  Copyright (c) 2014 Piri-Piri. All rights reserved.
+//
+
+import SpriteKit
+
+class SettingsMenuScene: SKScene {
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
